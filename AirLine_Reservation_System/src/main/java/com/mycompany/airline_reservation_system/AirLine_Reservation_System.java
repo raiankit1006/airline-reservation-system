15af84b35223e4f40648d417f10c89de76d277f2
raiankit1006 <115402024+raiankit1006@users.noreply.github.com>
2022-12-29 14:17:11 +0530

    Add files via upload

---
+++ AirLine_Reservation_System/src/main/java/com/mycompany/airline_reservation_system/AirLine_Reservation_System.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.airline_reservation_system;
+
+/**
+ *
+ * @author HP
+ */
+public class AirLine_Reservation_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
